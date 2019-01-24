db6cc796b82e64b7d75accd430749d958d1e71a5
Anthony Ho <anwyho@gmail.com>
2019-01-23 21:19:59 -0800

    Added Button to compose.send API.

--- compose/send/__init__.py
+++ compose/send/__init__.py
@@ -1,9 +1,10 @@
 from typing import (List)
 
+from .button import (Button)
 from .response import (InvalidResponseStructureError, Response)
 from .response_attachment import (Asset, Template, ResponseAttachment)
 from .response_builder import (ResponseBuilder, ResponseBuilderError)
 
-__all__: List[str] = ['Asset', 'InvalidResponseStructureError', 'Response',
-                      'ResponseAttachment', 'ResponseBuilder',
+__all__: List[str] = ['Asset', 'Button', 'InvalidResponseStructureError',
+                      'Response', 'ResponseAttachment', 'ResponseBuilder',
                       'ResponseBuilderError', 'Template']
